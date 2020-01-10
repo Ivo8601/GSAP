2105a36de009e922f8a6431b45655298c39e6d6b
Ivo <32270959+Ivo8601@users.noreply.github.com>
2020-01-10 13:05:20 +0000

    Final copy
    Final stage of gsap test in action - maximum load and fps count

--- main2.js
+++ main2.js
@@ -1,4 +1,5 @@
-  // BOUNCING BALL
+  
+ //---------------------------------------------------------------BOUNCING BALL
 function jumpBox(interval, canvas) {
     var ctx = canvas.getContext("2d");
     var ballRadius = 20;
@@ -41,15 +42,17 @@ function jumpBox(interval, canvas) {
   return setInterval(draw, interval);
 }
 
-// MAIN GSAP FUNCTION
+//-------------------------------------------------------------- MAIN GSAP FUNCTION
 function GSAP() {
   var borderLine = document.querySelector('.border');
   var mainHead = document.querySelector('.main__head');
   var subHead = document.querySelector('.sub__head');
   var logo = document.querySelector('.svg');
+  var stagger = document.querySelector('.stagger__effect');
 
-  // GSAP ANIMATIONS
-  // GSAP functions
+
+  //------------------------------------------------------------GSAP ANIMATIONS
+  // CIRCLE SPIN
   function circlesSpin() {
   gsap.to(".circ__1", { rotate: 360, x: 100, y: 150, repeat: -1, duration: 13, yoyo: true });
   gsap.to(".circ__2", { rotate: 250, x: 25, y: 40, repeat: -1, duration: 15, yoyo: true });
@@ -58,50 +61,124 @@ function GSAP() {
   gsap.to(".circ__5", { rotate: 250, y: 40, repeat: -1, duration: 18, yoyo: true });
 }
 
-  function projects() {
 
-        var tlTwo = gsap.timeline({repeat: 0});
-
-        tlTwo.to(logo, { duration: 2, y: -200, opacity: 0, ease: "bounce.in(3)" })
-        .to('.item, nav', { duration: 1.5, y: -200, opacity: 0, stagger: 0.3 })
-        .to(mainHead, { duration: 1, opacity: 0, delay: -2 })
-        .to(subHead, { duration: 1, x: -1200, opacity: 0, scale: 0.1 })
-        .to(borderLine, { duration: 2.5, rotate: 90, delay: 0.5, width: '0%' });
-  
+  //----------------------------------------------------------------GSAP STAGGER
+function staggersStart() {
+    gsap.to('.stagger', { repeat: -1, duration: 0.5, scale: 0, y: 40, ease:"power1.out", yoyo: true, stagger: {
+      grid: [1,9],
+      from: "left",
+      amount: 3
+    }});
   }
 
 
-  //Timeline
+  //Stagger function
+function staggers() {
+  $(stagger).on('click', function() {
+    var tlThree = gsap.timeline({repeat: 0, yoyo: true});
+    tlThree.to('.stagger', { duration: 0.5, opacity: 1, stagger: {
+      grid: [1,9],
+      from: "center",
+      amount: 3
+    }})
+    .to('.stag__btn', { duration: 2, opacity: 1 });
+  });
+}
+function centerBtn() {
+  $('.center__btn').on('click', function() {
+    staggersStart();
+  }
+)
+}
+
+
+ //---------------------------------------------------------------BOUNCING BOX
+ function bounceBox() {
+   var tlFour = gsap.timeline({repeat: 0});
+    tlFour.to('.box__container', { duration: 1, display: "block" })
+    .from('.canvas', { y: -1500, rotate: 360, opacity: 0, duration: 3, stagger: 1 })
+    .from('.box__head', { opacity: 1, duration: 2, stagger: 1 });
+}
+function animateBox() {
+  var tlFive = gsap.timeline({repeat: -1, repeatDelay: 0.5, yoyo: true});
+  var canvas = document.querySelectorAll('.canvas');
+  tlFive.to(canvas, { duration: 3, rotate: 360, stagger: {from: "center", amount: 2}, scale: 0.3, borderRadius: "100%"})
+  .to(canvas, { duration: 2, boxShadow: "3px 3px 30px red", yoyo: true });
+}
+
+function dropBox() {
+  $('.box__effect').on('click', function() {
+      bounceBox();
+  });
+  $('.animate').on('click', function() {
+    animateBox();
+});
+}
+
+//-------------------------------------------------------------------FPS COUNT
+
+$('.render').on('click', function() {
+  
+  var fpsTL = gsap.timeline({repeat: 0});
+  var fps = document.querySelector('#fps');
+
+  fpsTL.to(fps, {duration: 5, opacity: 1, ease: "elastic.out(4.5)"});
+
+});
+
+function fpsCount() {
+  var fps = document.getElementById("fps"),
+    startTime = Date.now(),
+    frame = 0;
+
+function tick() {
+  var time = Date.now();
+  frame++;
+  if (time - startTime > 1000) {
+      fps.innerHTML = (frame / ((time - startTime) / 1000)).toFixed(1) + ' fps';
+      startTime = time;
+      frame = 0;
+}
+  window.requestAnimationFrame(tick);
+}
+tick();
+}
+
+
+
+
+
+  //--------------------------------------------------------------MAIN TIMELINE
   var tl = gsap.timeline({repeat: 0, repeatDelay: 0.5 });
-  //General variables
- 
+
 
   //Animate - top border
   tl.from(borderLine, { y: -300, opacity: 0.2, duration: 2, delay: 0.5, });
   tl.to(borderLine, { width: '20%', duration: 1 })
-   .to(borderLine, { rotate: 360, duration: 1, delay: 0.3  })
-   .to(borderLine, { width: '80%', duration: 2.5, delay: 0.5 });
+   .to(borderLine, {  duration: 2.5, delay: 0.3, width: '80%', ease: "elastic.out(1, 0.3)" });
    //Animate - top nav
   tl.from(".item, nav", { y: -200, duration: 1.5, stagger: 0.3, opacity: 0, delay: -2 });
   //Animate - heading + subheading
   tl.from(mainHead, {  duration: 3, opacity: 0, delay: 1 })
-  .from(subHead, { x: -1200, duration: 1, ease: "back.out(2.5)" });
+  .from(subHead, { x: -1200, duration: 1, ease: "back.out(2.5)", delay: 0 });
   //Animation - logo
-  tl.from(logo, { y: -200, opacity: 0, ease: "elastic.out(1)", duration: 2, delay: 2 });
+  tl.from(logo, { y: -200, opacity: 0, ease: "elastic.out(1)", duration: 2, delay: 0 });
   //Animate - circles
   tl.to(".circle", { opacity: 0.3, duration: 5, delay: 0.7 });
   // Animate - logo loop
   tl.to(logo, { duration: 2, scale: 1.1, ease: "elastic", yoyo: true, repeat: -1, delay: 0.5 });
 
+  staggers();
   circlesSpin();
-  $('.projects').on('click', function() {
-      projects();
-  });
-
+  centerBtn();
+  dropBox();
+  fpsCount();
 }
 
-$(document).ready(function() {
 
+
+//-------------------------------------------------------------------ON LOAD
+$(document).ready(function() {
 
 arr = document.getElementsByClassName('canvas');
 // Loop through jumpBox 
